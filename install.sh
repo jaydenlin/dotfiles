d61a872ae76d3b9fb80809c626ec802c5b642c5f
jayden <jayden@yahoo-inc.com>
2016-06-28 00:46:45 +0800

    add install codes

---
+++ install.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+cd "$(dirname "${BASH_SOURCE}")";
+
+function doIt() {
+	rsync --exclude ".git/" \
+		--exclude ".DS_Store" \
+		--exclude ".osx" \
+		--exclude "install.sh" \
+		--exclude "README.md" \
+		-avh --no-perms . ~;
+	source ~/.bashrc;
+}
+
+if [ "$1" == "--force" -o "$1" == "-f" ]; then
+	doIt;
+else
+	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
+	diff ./.bashrc ~/.bashrc
+        
+        echo "";
+	if [[ $REPLY =~ ^[Yy]$ ]]; then
+		doIt;
+	fi;
+fi;
+unset doIt;
